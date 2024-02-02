7485dfe02668abdce2ac5bfcf7c59663ff902a57
rio internship <rio.ferdinand.internship@datanusantara.com>
2024-02-02 23:04:51 +0700

    projek

---
+++ resources/views/index.blade.php
@@ -0,0 +1,272 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+    <title>Data Diri</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-slider.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+</head>
+
+<body>
+    <div id="header-holder" class="main-header">
+        <div class="bg-animation">
+            <div class="graphic-show">
+                <img class="fix-size" src="images/graphic1.png" alt="">
+                <img class="img img1" src="images/graphic1.png" alt="">
+                <img class="img img2" src="images/graphic2.png" alt="">
+                <img class="img img3" src="images/graphic3.png" alt="">
+            </div>
+        </div>
+        <nav id="nav" class="navbar navbar-default navbar-full">
+            <div class="container-fluid">
+                <div class="container container-nav">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="navbar-header">
+                                <button aria-expanded="false" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="logo-holder" href="/">
+                                    <!-- <div class="logo" style="width:62px;height:18px"></div> -->
+                                    <div>Rio Ferdinand</div>
+                                </a>
+                            </div>
+                            <div style="height: 1px;" role="main" aria-expanded="false" class="navbar-collapse collapse" id="bs">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li><a href="#top-content">Home</a></li>
+                                    <li><a href="#footer">About me</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div id="top-content" class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div id="main-slider">
+                            <div class="slide">
+                                <!-- <div class="noti-holder">
+                                <a href="#">
+                                    <div class="noti">
+                                        <span class="badge">New</span><span class="text">Added new packages in cloud hosting.</span>
+                                    </div>
+                                </a>
+                            </div> -->
+                                <div class="spacer"></div>
+                                <div class="big-title">Nama Saya,<span>Rio Ferdinand</span><br></div>
+                                <p>May the beauty of this World, <span class="color:#84ebad;;">Always Shine.</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="info" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="info-text">Halo! nama saya Rio Ferdinand dan saya adalah Software Engineering pemula dan berharap untuk menambah soft skill dan hardskill yang lebih.</div>
+                    <button class="ybtn ybtn-accent-color ybtn-shadow" onclick="alertGaada()">Disini ga ada apa apa btw.</button>
+                    <!-- <a href="signup.html" class="ybtn ybtn-accent-color ybtn-shadow">Disini ga ada apa apa btw.</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="services" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row-title">My Project</div>
+                    <div class="row-subtitle">Project yang pernah dibuat</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <img src="images/service-icon1.svg" alt="">
+                        </div>
+                        <div class="service-title"><a href="webhosting.html">Steam Cashier</a></div>
+                        <div class="service-details">
+                            <p>Di web ini sang user diberi langkah-langkah untuk mereservasi untuk mencuci kendaraan dan disini juga terdapat sebuah sistem untuk bayar langsung ataupun dengan online.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <img src="images/service-icon2.svg" alt="">
+                        </div>
+                        <div class="service-title"><a href="#">Internship Project</a></div>
+                        <div class="service-details">
+                            <p>Website ini dibuat ketika saya melakukan magang di sebuah perusahaan start-up, dan saya diberi perintah untuk membuat website yang digunakan untuk absensi para karyawan beserta menghitungan kalkulasinya
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <img src="images/service-icon3.svg" alt="">
+                        </div>
+                        <div class="service-title"><a href="vpshosting.html">Public Complaint</a></div>
+                        <div class="service-details">
+                            <p>Di website ini diperuntukkkan untuk 3 user yaitu rakyat sebagai yang mengadu,petugas sebagai penanggap, dan admin yang merngurus semuanya dari belakang.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <img src="images/service-icon4.svg" alt="">
+                        </div>
+                        <div class="service-title"><a href="cloudhosting.html">Fandom Page</a></div>
+                        <div class="service-details">
+                            <p>Ini adalah website pertama saya, yaitu website tentang sebuah group band anime bernama BanG Dream dan saya membuat sebuah website untuk para fans, tetapi saya Hold sampai sekarang.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="testimonials" class="container-fluid">
+        <div class="bg-color"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="row-title">Sertificates</div>
+                    <div class="row-subtitle">I dont have Certifates so Im using a Dummy Pictures</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- <div id="testimonials-slider">
+                        <div>
+                            <div class="details-holder">
+                                <img class="photo" src="images/person1.jpg" alt="">
+                                <h4>----</h4>
+                                <p> -----
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="details-holder">
+                                <img class="photo" src="images/person1.jpg" alt="">
+                                <h4>----</h4>
+                                <p> -----
+                                </p>
+                            </div>
+                        </div>
+                    </div> -->
+                    <div class="slider-wrapper">
+                        <div class="slider">
+                            <img id="slide-1" src="assets/images/sohee1.jpg">
+                            <img id="slide-2" src="assets/images/sohee2.jpg">
+                            <img id="slide-3" src="assets/images/sohee3.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="more-features" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row-title">My Goal</div>
+                    <div class="row-subtitle">This is my goal for now</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="mfeature-box">
+                        <div class="mfeature-icon">
+                            <i class="htfy htfy-trophy"></i>
+                        </div>
+                        <div class="mfeature-title">Trying to reach an experiment</div>
+                        <div class="mfeature-details">Mencoba untuk Mengikuti beberapa lomba agar dapat meraih pengalaman.</div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="mfeature-box">
+                        <div class="mfeature-icon">
+                            <i class="htfy htfy-like"></i>
+                        </div>
+                        <div class="mfeature-title">Trying to help each other</div>
+                        <div class="mfeature-details">Mencoba untuk membantu sesama dan memiliki nama baik di kalangan sesama.</div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="mfeature-box">
+                        <div class="mfeature-icon">
+                            <i class="htfy htfy-cogwheel"></i>
+                        </div>
+                        <div class="mfeature-title">Be a Full-stack Developer</div>
+                        <div class="mfeature-details">Mencoba untuk menjadi Fullstack Dev secara otodidak.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-md-6">
+                    <div class="address-holder" style="margin-top:5px;">
+                        <div class="phone"><i class="fas fa-phone"></i> 08996810044</div><br>
+                        <div class="email"><i class="fas fa-envelope"></i>rioferdinandf53327@gmail.com</div><br>
+                        <div class="address">
+                            <i class="fas fa-map-marker"></i>
+                            <div>Bogor City, Gudang Garam,<br>
+                                East Bogor, West Java,<br>
+                                Indonesia.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="social-menu-holder">
+                        <ul class="social-menu">
+                            <li><a style="text-decoration:none;margin-left: 250px;" href="https://www.instagram.com/leofinhare/"><i class="fa-brands fa-instagram"></i> leofinhare</a></li><br>
+                            <li><a style="text-decoration:none;margin-left: 250px;" href="https://github.com/RioFerdinand"><i class="fa-brands fa-github"></i> Rio Ferdinand</a></li><br>
+                            <li><a style="text-decoration:none;margin-left: 250px;margin-top:13px;" href="https://www.linkedin.com/in/rio-ferdinand-a8653424b/"><i class="fa-brands fa-linkedin"></i> Rio Ferdinand</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="https://kit.fontawesome.com/261be840cc.js" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script>
+        function alertGaada() {
+            Swal.fire({
+                title: 'Hehe!',
+                text: 'Dibilang gaada apa apa.',
+                imageUrl: 'https://th.bing.com/th/id/OIP.-6bs1dL3x5JRd7zrIisJsQHaHa?pid=ImgDet&rs=1',
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
+            })
+        }
+    </script>
+</body>
+
+</html>
